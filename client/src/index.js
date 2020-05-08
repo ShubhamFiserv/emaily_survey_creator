9e01944f114d46fc264c22ea2d3584b6cd5a0c8e
shubham shukla <shubhamshukla14@gmail.com>
2020-05-08 22:03:06 +0530

    some last minutes change

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,6 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-import axios from 'axios';
-window.axios = axios;
 
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
@@ -19,7 +17,4 @@ ReactDOM.render(
    <Provider store={store}><App /></Provider>,
    document.querySelector('#root')
 );
-
-console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
-console.log('Environment is', process.env.NODE_ENV);
 
