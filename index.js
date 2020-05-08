6f09b63e2c44dc681236b43568bb4c3e9fd44679
shubham shukla <shubhamshukla14@gmail.com>
2020-05-08 21:54:43 +0530

    Users can see survey results

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const  passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 const app = express();
@@ -25,6 +26,7 @@ mongoose.connect(keys.mongoURI,{useNewUrlParser: true});
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if(process.env.NODE_ENV === 'production'){
    // Express will serve up production assets
